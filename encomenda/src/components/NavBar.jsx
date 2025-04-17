ce24a820d4cf5097e263e5ed13af587e9c3b1f96
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-04-17 10:49:19 -0300

    agr sim, OK

--- encomenda/src/components/NavBar.jsx
+++ encomenda/src/components/NavBar.jsx
@@ -8,6 +8,12 @@ function NavBar(){
          
             <Link to="/">🏡Home</Link>
             <br/>
+            <Link to="/Detalhes1">🍔Lanche</Link>
+            <br/>
+            <Link to="/Detalhes2">🌭Hot-Dog</Link>
+            <br/>
+            <Link to="/Detalhes3">🍱Marmitex</Link>
+            <br/>
          
         </nav>
         </header>
