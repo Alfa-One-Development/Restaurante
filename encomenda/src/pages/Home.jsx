b87e5a3f5334e416f8f9bb49b1747ddb7d348d70
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-04-17 10:22:09 -0300

    Versão adequada

--- encomenda/src/pages/Home.jsx
+++ encomenda/src/pages/Home.jsx
@@ -1,48 +1,23 @@
 import Header from "../components/Header";
-import Footer from "../components/footer"
+import Footer from "../components/Footer"
 import Produtos from "../components/Produto";
-
-import { Carousel } from "react-bootstrap";
-
 import Combo from "../assets/Combo1.png";
 import HotDog from "../assets/Hot-Dog2.png";
 import Marmitex from "../assets/Marmitex.png";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
 
   return (
     <>
-      <Header/>
+      <Header />
       <section className="Main">
-      <Carousel >
-            <Carousel.Item>
-              <img
-                src={Combo}
-                loading="lazy"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                src={HotDog}
-                loading="lazy"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                src={Marmitex}
-                loading="lazy"
-              />
-            </Carousel.Item>
+        <h1>Home Page</h1>
 
-          </Carousel>
-
-          <Produtos/>
+      <Produtos />
 
       </section>
 
-      <Footer/>
+      <Footer />
     </>
   )
 }
