1d045bae8a0532082780820ce4601ce6904d7c0f
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-04-17 10:20:24 -0300

    eu

--- encomenda/src/pages/Detalhes1.jsx
+++ encomenda/src/pages/Detalhes1.jsx
@@ -2,9 +2,9 @@ function Lanche() {
 
     return (
         <div>
-      <h1>X-Ratão</h1>
-      <p>Detalhes do Lanche...</p>
-      </div>
+            <h1>Combo X-Ratão</h1>
+            <p>Ingredientes : Pão com Gergilim, Cebola Crispy, Maionese Caseira, 2 Hamburgueres, Queijo, Bacon em Fatias, 100G DE Batata Frita e Uma lata de Coca-Cola 350ml.</p>
+        </div>
     )
 }
 
--- encomenda/src/pages/Detalhes2.jsx
+++ encomenda/src/pages/Detalhes2.jsx
@@ -3,7 +3,7 @@ function HotDog() {
     return (
         <div>
       <h1>Dogão</h1>
-      <p>Detalhes do Hot-Dog...</p>
+      <p>Ingredientes : Pão, 2 Salcichas, Purê de Batata, Vinagrete, Queijo Ralado, Batata Palha, Bacon em Cubos e Catupiry </p>
       </div>
     )
 }
