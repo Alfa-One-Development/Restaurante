d4f18bef0454e4b764585fcc3dd56b6213654d20
Vitor Izidoro <vitorizidoro288@gmail.com>
2025-04-17 10:54:26 -0300

    Nada

--- encomenda/src/pages/Detalhes2.jsx
+++ encomenda/src/pages/Detalhes2.jsx
@@ -6,6 +6,7 @@ function HotDog() {
       <p>Ingredientes : Pão, 2 Salcichas, Purê de Batata, Vinagrete, Queijo Ralado, Batata Palha, Bacon em Cubos e Catupiry </p>
       </div>
     )
+
 }
 
 export default HotDog
