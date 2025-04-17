8848e97a998ac0cb15a4096cc283688283574447
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-04-17 09:56:37 -0300

    detalhes

---
+++ encomenda/src/pages/Detalhes1.jsx
@@ -0,0 +1,11 @@
+function Lanche() {
+
+    return (
+        <div>
+      <h1>X-Ratão</h1>
+      <p>Detalhes do Lanche...</p>
+      </div>
+    )
+}
+
+export default Lanche
---
+++ encomenda/src/pages/Detalhes2.jsx
@@ -0,0 +1,11 @@
+function HotDog() {
+
+    return (
+        <div>
+      <h1>Dogão</h1>
+      <p>Detalhes do Hot-Dog...</p>
+      </div>
+    )
+}
+
+export default HotDog
